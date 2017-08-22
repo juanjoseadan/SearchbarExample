a3dc45fd0f50299596c8be99b63b7e80184b79e4
MacBook-Pro\ISCAd <isc.adanlopez@gmail.com>
2017-08-22 13:05:04 -0500

    Searchbar added to first page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,18 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  public keyword: string = '';
+
   constructor(public navCtrl: NavController) {
 
   }
 
+
+  search() {
+    console.log('Searching this keyword: ' + this.keyword);
+  }
+
+  cancelSearch(t, evt) {
+    console.log('Here I need to close the keyboard, but it keeps opening every time you hit the Cancel button... You need to tap in the ion content to close the keyboard');
+  }
 }
